6cbd17fa744199a00f85bb2c8f3dce4cad5b7221
Paluffel <40839103+Paluffel@users.noreply.github.com>
2020-06-02 20:37:17 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -63,7 +63,7 @@ export default class HelloWorld {
                     },
                     transform: {local: {
                         position: { x: 0, y: 0, z: 0 },
-                        scale: { x: 1, y: 1, z: 1},
+                        scale: { x: 1.06, y: 1.06, z: 1.06},
                        
                     }}
                 }
